7661f333ac93840e54ac1d465a3c9b7792c03276
juanmanuelug <juanmanuelug2000@gmail.com>
2022-08-17 20:17:09 +0200

    Order enemy Missiles by distance in the countermeasure systems

--- CounterMeasureSystem.py
+++ CounterMeasureSystem.py
@@ -29,7 +29,8 @@ class CounterMeasureSystemClass(object):
                 self.enemyMissilesAssigned.pop(enemyMissile)
 
     def launchCounterMeasure(self, counterMeasuresMissiles, actualTime):
-        if len(self.enemyMissilesAssigned) > 0:
+        if len(self.enemyMissilesAssigned) > 0 and len(self.enemyMissilesAssigned) != len(self.enemyMissileIdAssignedToCounterMeasureMissileId):
+            self.sortEnemyMissilesByDistance()
             for enemyMissile in list(self.enemyMissilesAssigned):
                 if enemyMissile not in self.enemyMissileIdAssignedToCounterMeasureMissileId:
                     if (actualTime - self.lastLaunchTime) / 1000 > 1:
@@ -42,3 +43,41 @@ class CounterMeasureSystemClass(object):
                         self.enemyMissileIdAssignedToCounterMeasureMissileId.update({enemyMissile: self.idCounterMeasuresMissiles})
                         self.lastLaunchTime = actualTime
                         self.enemyMissilesAssigned.pop(enemyMissile)
+
+    def sortEnemyMissilesByDistance(self):
+        enemyIdArray = []
+        enemyPosition = []
+        enemyDict = defaultdict(list)
+        for enemyId in list(self.enemyMissilesAssigned):
+            enemyIdArray.append(enemyId)
+            enemyPosition.append(self.enemyMissilesAssigned[enemyId])
+
+        i = 0
+        j = i + 1
+        while i < len(self.enemyMissilesAssigned):
+            xDist = self.position.positionX - enemyPosition[i][0]
+            yDist = self.position.positionY - enemyPosition[i][1]
+            zDist = self.position.positionZ - enemyPosition[i][2]
+            dist = math.sqrt((xDist * xDist) + (yDist * yDist) + (zDist * zDist))
+            while j < len(self.enemyMissilesAssigned):
+                xDistNew = self.position.positionX - enemyPosition[j][0]
+                yDistNew = self.position.positionY - enemyPosition[j][1]
+                zDistNew = self.position.positionZ - enemyPosition[j][2]
+                distNew = math.sqrt((xDistNew * xDistNew) + (yDistNew * yDistNew) + (zDistNew * zDistNew))
+                if distNew < dist:
+                    auxPos = enemyPosition[j]
+                    enemyPosition[j] = enemyPosition[i]
+                    enemyPosition[i] = auxPos
+                    auxId = enemyIdArray[j]
+                    enemyIdArray[j] = enemyIdArray[i]
+                    enemyIdArray[i] = auxId
+                    dist = distNew
+                j += 1
+            i += 1
+
+        i = 0
+        while i < len(enemyIdArray):
+            enemyDict[enemyIdArray[i]] = enemyPosition[i]
+            i += 1
+        self.enemyMissilesAssigned = enemyDict
+
